9f29936c3f0263d0834af2d6449ad57a93b8dd1d
immijimmi <imranhamid99@msn.com>
2022-07-16 00:51:51 +0100

    Removed testcoverage (no tests to run)

--- tools/testcoverage.bat
+++
@@ -1,8 +0,0 @@
-:: Requires pytest and coverage to be installed
-:: This script will run any test files it discovers in the directory and provide a breakdown of code coverage
-
-cd ..
-coverage run -m --source=. --omit="test\*,setup.py" py.test -v
-coverage report
-
-pause
